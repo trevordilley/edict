1ea9be2752d775f813912a98d0099fc37512989e
trevordilley <trevor.lee.dilley@gmail.com>
2022-09-08 20:48:50 -0700

    Moar tests

--- packages/rete/src/lib/rete.spec.ts
+++ packages/rete/src/lib/rete.spec.ts
@@ -7,11 +7,6 @@ enum Id {
   Seth, Thomas, Xavier, Yair, Zach,
   Derived,
 }
-enum Attr {
-  Color, LeftOf, RightOf, Height, On, Age, Self,
-  AllPeople,
-}
-
 
 interface SmallSchema {
   Color: string
@@ -198,4 +193,129 @@ describe('rete', () => {
 
 
   });
+
+
+  it('updating facts in different alpha nodes', () => {
+    const session = rete.initSession<SmallSchema>(false)
+    const production = rete.initProduction<SmallSchema, MatchT<SmallSchema>>({
+        name: "updatingFacts",
+        convertMatchFn,
+      }
+    )
+
+    rete.addConditionsToProduction(production, {name: "b", field: Field.IDENTIFIER}, "Color", "blue", true)
+    rete.addConditionsToProduction(production, {name: "y", field: Field.IDENTIFIER}, "LeftOf", Id.Zach, true)
+    rete.addConditionsToProduction(production, {name: "a", field: Field.IDENTIFIER}, "Color", "maize", true)
+    rete.addConditionsToProduction(production, {name: "y", field: Field.IDENTIFIER}, "RightOf", {name: "b", field: Field.VALUE}, true)
+    rete.addProductionToSession(session, production)
+    rete.insertFact(session,[Id.Bob, "Color", "blue"])
+    rete.insertFact(session,[Id.Yair, "LeftOf", Id.Zach])
+    rete.insertFact(session,[Id.Alice, "Color", "maize"])
+    rete.insertFact(session,[Id.Yair, "RightOf", Id.Bob])
+    rete.fireRules(session)
+    const results = rete.queryAll(session, production)
+    expect(results.length).toBe(1)
+
+    rete.insertFact(session,[Id.Yair, "LeftOf", Id.Xavier])
+    rete.fireRules(session)
+    const newResults = rete.queryAll(session, production)
+    expect(newResults.length).toBe(0)
+
+
+  });
+
+  it('facts can be stored in multiple alpha nodes', () => {
+    const session = rete.initSession<SmallSchema>(false)
+    let alice: Id
+    let zach: Id
+    const rule1 = rete.initProduction<SmallSchema, MatchT<SmallSchema>>({
+        name: "rule1",
+        convertMatchFn,
+        thenFn:({vars}) => {
+          alice = vars.getValue("a") as Id
+        }
+      }
+    )
+    rete.addConditionsToProduction(rule1, {name: "a", field: Field.IDENTIFIER}, "LeftOf", Id.Zach, true)
+    rete.addProductionToSession(session, rule1)
+    const rule2 = rete.initProduction<SmallSchema, MatchT<SmallSchema>>({
+        name: "rule2",
+        convertMatchFn,
+        thenFn:({vars}) => {
+          zach = vars.getValue("z") as Id
+        }
+      }
+    )
+    rete.addConditionsToProduction(rule2, {name: "a", field: Field.IDENTIFIER}, "LeftOf", {name: "z", field: Field.VALUE}, true)
+    rete.addProductionToSession(session, rule2)
+    rete.insertFact(session, [Id.Alice, "LeftOf", Id.Zach])
+    rete.fireRules(session)
+    // @ts-ignore
+    expect(alice).toBe(Id.Alice)
+    // @ts-ignore
+    expect(zach).toBe(Id.Zach)
+  });
+
+
+  it('multiple joins', () => {
+    const session = rete.initSession<SmallSchema>(false)
+    const rule1 = rete.initProduction<SmallSchema, MatchT<SmallSchema>>({
+        name: "rule1",
+        convertMatchFn,
+      condFn: (vars) => {
+          return vars.getValue("id2") != vars.getValue("id1")
+      },
+      thenFn: ({vars, session}) => {
+         rete.insertFact(session, [vars.getValue("id2")! as Id, "Color", "red"])
+        rete.insertFact(session, [vars.getValue("id2")! as Id, "Height", 72])
+      }
+
+      })
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "LeftOf", Id.Bob, true)
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "Color", {name: "color", field: Field.VALUE}, true)
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "Height", {name: "height", field: Field.VALUE}, true)
+    rete.addConditionsToProduction(rule1, {name: "id2", field: Field.IDENTIFIER}, "LeftOf", {name: "leftOf", field: Field.VALUE}, true)
+    rete.addConditionsToProduction(rule1, {name: "id2", field: Field.IDENTIFIER}, "Color", {name: "color2", field: Field.VALUE}, false)
+    rete.addConditionsToProduction(rule1, {name: "id2", field: Field.IDENTIFIER}, "Height", {name: "height2", field: Field.VALUE}, false)
+    rete.addProductionToSession(session, rule1)
+
+    rete.insertFact(session, [Id.Alice, "LeftOf", Id.Bob])
+    rete.insertFact(session, [Id.Alice, "Color", "blue"])
+    rete.insertFact(session, [Id.Alice, "Height", 60])
+    rete.insertFact(session, [Id.Bob, "LeftOf", Id.Charlie])
+    rete.insertFact(session, [Id.Bob, "Color", "green"])
+    rete.insertFact(session, [Id.Bob, "Height", 70])
+
+    rete.fireRules(session)
+
+    expect(rete.queryAll(session, rule1).length).toBe(1)
+  });
+
+  it('join followed by non-join', () => {
+    const session = rete.initSession<SmallSchema>(false)
+    const rule1 = rete.initProduction<SmallSchema, MatchT<SmallSchema>>({
+        name: "rule1",
+        convertMatchFn,
+      }
+    )
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "LeftOf", Id.Bob, true)
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "Color", {name: "color", field: Field.VALUE}, true)
+    rete.addConditionsToProduction(rule1, {name: "id1", field: Field.IDENTIFIER}, "Height", {name: "height", field: Field.VALUE}, true)
+    rete.addConditionsToProduction(rule1, Id.Bob, "RightOf", {name: "a", field: Field.VALUE}, true)
+    rete.addProductionToSession(session, rule1)
+
+    rete.insertFact(session, [Id.Bob, "RightOf", Id.Alice])
+    rete.insertFact(session, [Id.Alice, "LeftOf", Id.Bob])
+    rete.insertFact(session, [Id.Alice, "Color", "blue"])
+    rete.insertFact(session, [Id.Alice, "Height", 60])
+    rete.insertFact(session, [Id.Charlie, "LeftOf", Id.Bob])
+    rete.insertFact(session, [Id.Charlie, "Color", "green"])
+    rete.insertFact(session, [Id.Charlie, "Height", 70])
+
+    rete.fireRules(session)
+
+    expect(rete.queryAll(session, rule1).length).toBe(2)
+  });
+
+
 });
