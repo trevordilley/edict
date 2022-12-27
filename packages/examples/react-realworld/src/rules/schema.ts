6391f1b2d6b3f741789cf9be89eb6a690d1bb9e3
Trevor Dilley <trevor@Trevors-2021-M1-Pro.local>
2022-12-27 08:49:29 -0800

    Cleans up schema for realworld example a bit

--- packages/examples/react-realworld/src/rules/schema.ts
+++ packages/examples/react-realworld/src/rules/schema.ts
@@ -1,31 +1,31 @@
-import { Article } from '../types/article';
-import { User, UserSettings } from '../types/user';
-import { Profile } from '../types/profile';
-import { Comment } from '../types/comment';
+import { Article } from '../types/article'
+import { User, UserSettings } from '../types/user'
+import { Profile } from '../types/profile'
+import { Comment } from '../types/comment'
 
 export const ID = {
   ARTICLE: (article: Article) => article.slug,
   COMMENT: (id: number) => `comment${id}`,
   PROFILE: (profile: Profile) => profile.username,
-} as const;
+} as const
 
 interface Error {
-  errors: { [key: string]: string[] };
+  errors: { [key: string]: string[] }
 }
 
 export const HOME_TAB = {
   GLOBAL_FEED: 'Global Feed',
   YOUR_FEED: 'Your Feed',
-};
+}
 interface HomePage {
-  selectedTab: string;
-  tabNames: string[];
+  selectedTab: string
+  tabNames: string[]
 }
 
 interface ArticleList {
-  currentPage: number;
-  offset: number;
-  limit: number;
+  currentPage: number
+  offset: number
+  limit: number
 }
 
 export enum FetchState {
@@ -36,23 +36,28 @@ export enum FetchState {
 }
 
 export interface Fetch {
-  fetchState: FetchState;
+  fetchState: FetchState
+  isSubmitting: boolean
+  submittingFavorite: FetchState
+  submittingFollow: FetchState
+  deletingArticle: FetchState
+  isFavoriting: FetchState
+  submittingComment: FetchState
+  isLoggingIn: FetchState
 }
 
-export type Schema = Article &
+type ArticleSchema = Article &
   ArticleList & {
-    isSubmitting: boolean;
-    submittingFavorite: FetchState;
-    submittingFollow: FetchState;
-    deletingArticle: FetchState;
-    isFavoriting: FetchState;
-    commentBody: string;
-    submittingComment: FetchState;
-    tag: string;
-    filterByAuthor: string;
-  } & Fetch &
-  User & {
-    isLoggingIn: FetchState;
-  } & Error & { location: string } & Profile &
+    commentBody: string
+    tag: string
+    filterByAuthor: string
+    articleCount: number
+  }
+
+export type Schema = ArticleSchema &
+  Fetch &
+  User &
+  Error & { location: string } & Profile &
   UserSettings &
-  Comment & { articleCount: number } & HomePage;
+  Comment &
+  HomePage
