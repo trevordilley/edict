be781db199bc698e5286840d5ad55b48f0727f19
Trevor Dilley <trevor@Trevors-2021-M1-Pro.local>
2023-01-13 07:10:30 -0800

    remove unneeded tests

--- packages/rete/src/lib/perf.spec.ts
+++ packages/rete/src/lib/perf.spec.ts
@@ -3,8 +3,6 @@ import { Field, MatchT, viz } from '@edict/rete'
 import { performance } from 'perf_hooks'
 import v8Profiler from 'v8-profiler-next'
 import * as fs from 'fs'
-import MurmurHash3 from 'imurmurhash'
-import * as imm from 'immutable'
 
 v8Profiler.setGenerateType(1)
 
@@ -138,221 +136,6 @@ describe('baseline measure of time', () => {
     expect(2).toBe(2)
   })
 
-  it('profile Map vs Immutable Map', () => {
-    const len = 10000
-    let m = new Map<string, string>()
-    let im = imm.Map()
-    for (let i = 0; i < len; i++) {
-      const s = `el${i}`
-      m.set(s, s)
-      im = im.set(s, s)
-    }
-
-    performance.mark('profile_jsmap_start')
-    for (let i = 0; i < len; i++) {
-      const x = new Map(m)
-    }
-    performance.mark('profile_jsmap_end')
-    performance.measure(
-      'profile_jsmap',
-      'profile_jsmap_start',
-      'profile_jsmap_end'
-    )
-    performance.mark('profile_imm_start')
-    for (let i = 0; i < len; i++) {
-      const s = `el${i}`
-      const x = im.asMutable()
-      im.set(s, s)
-    }
-    performance.mark('profile_imm_end')
-    performance.measure('profile_imm', 'profile_imm_start', 'profile_imm_end')
-
-    console.table(perfResults())
-  })
-
-  it('profile data access', () => {
-    let x = 0
-    const u = new Uint32Array(1)
-    u[0] = 0
-    const mp = new Map<string, number>([['a', 0]])
-    const arr = [0]
-    const y = { y: 0 }
-    const z = { z: { z: { z: 0 } } }
-    const iter = 5_000_000
-    const bu = performance.now()
-    for (let i = 0; i < iter; i++) {
-      // @ts-ignore
-      u[0] += 1
-    }
-    const au = performance.now()
-    const bm = performance.now()
-    for (let i = 0; i < iter; i++) {
-      // @ts-ignore
-      mp.set('a', mp.get('a') + 1)
-    }
-    const am = performance.now()
-    const ba = performance.now()
-    for (let i = 0; i < iter; i++) {
-      arr[0] += 1
-    }
-    const aa = performance.now()
-    const bx = performance.now()
-    for (let i = 0; i < iter; i++) {
-      x += 1
-    }
-    const ax = performance.now()
-
-    const by = performance.now()
-    for (let i = 0; i < iter; i++) {
-      y.y += 1
-    }
-    const ay = performance.now()
-
-    const bz = performance.now()
-    const zs = ['z', 'z', 'z']
-    // @ts-ignore
-    const e = { [zs[0]]: { [zs[1]]: { [z[2]]: 0 } } }
-    for (let i = 0; i < iter; i++) {
-      // @ts-ignore
-      e['z']['z']['z'] += 1
-    }
-    const az = performance.now()
-
-    console.table({
-      u: au - bu,
-      m: am - bm,
-      a: aa - ba,
-      x: ax - bx,
-      y: ay - by,
-      z: az - bz,
-    })
-    expect(2).toBe(2)
-  })
-
-  it('Using Dictionary vs using native map', () => {
-    profile('nativeMap', 'profiles/packages/rete', () => {
-      const match = {
-        id: 0,
-        match: new Map<string, string | number>([
-          ['Delta', 'Delta'],
-          ['dt', 1],
-        ]),
-        enabled: true,
-      }
-      const idAttrs = [
-        ['bob', 'age'],
-        ['sally', 'age'],
-        ['jim', 'age'],
-        ['tim', 'age'],
-        ['bob', 'name'],
-        ['sally', 'name'],
-        ['jim', 'name'],
-        ['tim', 'name'],
-      ]
-
-      const iterCount = 1_000_000
-
-      performance.mark('map_start')
-      for (let i = 0; i < iterCount; i++) {
-        const m = new Map<string, Map<string, typeof match>>()
-        for (const [id, attr] of idAttrs) {
-          if (!m.get(id)) m.set(id, new Map([[attr, match]]))
-          else m.get(id)?.set(attr, match)
-        }
-        for (const [id, attr] of idAttrs) {
-          m.get(id)?.delete(attr)
-          if (m.get(id)?.size === 0) m.delete(id)
-        }
-      }
-      performance.mark('map_end')
-      performance.measure('map', 'map_start', 'map_end')
-
-      const charsum = function (s: string) {
-        var i,
-          sum = 0
-        for (i = 0; i < s.length; i++) {
-          sum += s.charCodeAt(i) * (i + 1)
-        }
-        return sum
-      }
-
-      const array_hash = function (a: string[][]) {
-        var i,
-          sum = 0
-        for (i = 0; i < a.length; i++) {
-          var cs = charsum(a[i][0]) + charsum(a[i][1])
-          sum = sum + 65027 / cs
-        }
-        return ('' + sum).slice(0, 16)
-      }
-      performance.mark('hash_start')
-      for (let i = 0; i < iterCount; i++) {
-        array_hash(idAttrs)
-      }
-      performance.mark('hash_end')
-      performance.measure('hash', 'hash_start', 'hash_end')
-
-      performance.mark('toStr_start')
-      for (let i = 0; i < iterCount; i++) {
-        idAttrs.toString()
-      }
-      performance.mark('toStr_end')
-      performance.measure('toStr', 'toStr_start', 'toStr_end')
-
-      performance.mark('murmur_start')
-      for (let i = 0; i < iterCount; i++) {
-        const hash = MurmurHash3()
-        let result = 0
-        for (let idAttr of idAttrs) {
-          hash.hash(idAttr[0]).hash(idAttr[1])
-        }
-        if (result > 0) {
-          expect(result).toBe(hash.result())
-        } else {
-          result = hash.result()
-        }
-      }
-      performance.mark('murmur_end')
-      performance.measure('murmur', 'murmur_start', 'murmur_end')
-
-      const javaHash = function (idAttrs: string[][]) {
-        var hash = 0,
-          i,
-          j,
-          k,
-          chr
-        for (i = 0; i < idAttrs.length; i++) {
-          for (j = 0; j < idAttrs[i].length; j++) {
-            for (k = 0; k < idAttrs[i][j].length; k++) {
-              chr = idAttrs[i][j].charCodeAt(k)
-              hash = (hash << 5) - hash + chr
-              hash |= 0 // Convert to 32bit integer
-            }
-          }
-        }
-        return hash
-      }
-
-      performance.mark('java_start')
-      let jHash = 0
-      for (let i = 0; i < iterCount; i++) {
-        const result = javaHash(idAttrs)
-        if (jHash === 0) jHash = result
-        else if (jHash !== result)
-          throw new Error(`hash mismatch ${result} vs previous ${jHash}`)
-      }
-      performance.mark('java_end')
-      performance.measure('java', 'java_start', 'java_end')
-
-      const entries = performance
-        .getEntriesByType('measure')
-        .map((e) => `${e.name}: ${e.duration}`)
-        .join('\n')
-      console.log(entries)
-      expect(2).toBe(2)
-    })
-  })
-})
 
 describe('rete perf', () => {
   it('packed_5', () => {
@@ -484,22 +267,22 @@ describe('rete perf', () => {
     const NUM_ENTITIES = 100
     for (let i = 0; i < NUM_ENTITIES; i++) {
       const ab = `${i}ab`
-      rete.insertFact(session, [ab, 'A', 1])
+      rete.insertFact(session, [ab, 'A', 0])
       rete.insertFact(session, [ab, 'B', 1])
       const abc = `${i}abc`
-      rete.insertFact(session, [abc, 'A', 1])
+      rete.insertFact(session, [abc, 'A', 0])
       rete.insertFact(session, [abc, 'B', 1])
-      rete.insertFact(session, [abc, 'C', 1])
+      rete.insertFact(session, [abc, 'C', 2])
       const abcd = `${i}abcd`
-      rete.insertFact(session, [abcd, 'A', 1])
+      rete.insertFact(session, [abcd, 'A', 0])
       rete.insertFact(session, [abcd, 'B', 1])
-      rete.insertFact(session, [abcd, 'C', 1])
-      rete.insertFact(session, [abcd, 'D', 1])
+      rete.insertFact(session, [abcd, 'C', 2])
+      rete.insertFact(session, [abcd, 'D', 3])
       const abce = `${i}abce`
-      rete.insertFact(session, [abce, 'A', 1])
+      rete.insertFact(session, [abce, 'A', 0])
       rete.insertFact(session, [abce, 'B', 1])
-      rete.insertFact(session, [abce, 'C', 1])
-      rete.insertFact(session, [abce, 'E', 1])
+      rete.insertFact(session, [abce, 'C', 2])
+      rete.insertFact(session, [abce, 'E', 3])
     }
     rete.fireRules(session)
 
