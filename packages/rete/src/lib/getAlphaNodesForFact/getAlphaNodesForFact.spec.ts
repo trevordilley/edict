c9aa1500a2777a854b6d686aa0b61f641b808785
Trevor Dilley <trevor.lee.dilley@gmail.com>
2023-05-25 21:09:22 -0700

    Adds beginning of tests for getAlphaNodesForFact

---
+++ packages/rete/src/lib/getAlphaNodesForFact/getAlphaNodesForFact.spec.ts
@@ -0,0 +1,30 @@
+import { getAlphaNodesForFact } from './getAlphaNodesForFact'
+import { TestingSchema, testingSimpleSession } from '../testingUtils.spec'
+import { insertFact } from '../insertFact/insertFact'
+import { AlphaNode, Fact, Field } from '@edict/rete'
+
+describe('getAlphaNodesForFact...', () => {
+  it('A fact with the A attribute will have the one alpha node', () => {
+    const session = testingSimpleSession()
+    const fact: Fact<TestingSchema> = ['a', 'A', 1]
+    insertFact(session, fact)
+    const nodes = new Set<AlphaNode<TestingSchema>>()
+    getAlphaNodesForFact(session, session.alphaNode, fact, true, nodes)
+    // console.log(vizOnlineUrl(session))
+    expect(nodes.size).toBe(1)
+
+    const alphaNode = [...nodes][0]
+    expect(alphaNode.testField).toBe(Field.ATTRIBUTE)
+    expect(alphaNode.testValue).toBe('A')
+  })
+
+  it('A fact with the B attribute will have no alpha nodes', () => {
+    const session = testingSimpleSession()
+    const fact: Fact<TestingSchema> = ['b', 'B', 1]
+    insertFact(session, fact)
+    const nodes = new Set<AlphaNode<TestingSchema>>()
+    getAlphaNodesForFact(session, session.alphaNode, fact, true, nodes)
+    // console.log(vizOnlineUrl(session))
+    expect(nodes.size).toBe(0)
+  })
+})
---
+++ packages/rete/src/lib/testingUtils.spec.ts
@@ -0,0 +1,75 @@
+import { Field, MatchT } from '@edict/rete'
+import { initSession } from './initSession/initSession'
+import { initProduction } from './initProduction/initProduction'
+import { addProductionToSession } from './addProductionToSession/addProductionToSession'
+import { addConditionsToProduction } from './addConditionsToProduction/addConditionsToProduction'
+
+export interface TestingSchema {
+  A: number
+  B: number
+  C: number
+  D: number
+  E: number
+  F: number
+  G: number
+  H: number
+  I: number
+  J: number
+  K: number
+  L: number
+  M: number
+  N: number
+  O: number
+  P: number
+  Q: number
+  R: number
+  S: number
+  T: number
+  U: number
+  V: number
+  W: number
+  X: number
+  Y: number
+  Z: number
+  Data: number
+  delta: number
+}
+
+const convertMatchFn = (vars: MatchT<TestingSchema>) => vars
+export const testingSimpleSession = () => {
+  const session = initSession<TestingSchema>(false)
+  const makeProduction = (name: keyof TestingSchema) => {
+    const valName = name.toLowerCase()
+    const entJoin = '$ent'
+    const production = initProduction<TestingSchema, MatchT<TestingSchema>>({
+      name: `${name} production`,
+      convertMatchFn,
+      thenFn: ({ vars }) => {},
+    })
+    addConditionsToProduction(
+      production,
+      { name: 'Delta', field: Field.IDENTIFIER },
+      'delta',
+      { name: 'dt', field: Field.VALUE },
+      true
+    )
+    addConditionsToProduction(
+      production,
+      { name: entJoin, field: Field.IDENTIFIER },
+      name,
+      { name: valName, field: Field.VALUE },
+      false
+    )
+    addProductionToSession(session, production)
+    return production
+  }
+  makeProduction('A')
+  return session
+}
+
+describe('Simple testing session...', () => {
+  it('has one production', () => {
+    const session = testingSimpleSession()
+    expect([...session.leafNodes.values()].length).toBe(1)
+  })
+})
