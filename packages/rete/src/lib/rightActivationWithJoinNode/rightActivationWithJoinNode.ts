936cbb7388ee34c62f061f18d17b15419097e11c
Trevor Dilley <trevor.lee.dilley@gmail.com>
2023-05-29 21:28:41 -0700

    Adds fatty comment explaining the issue with this last for of

--- packages/rete/src/lib/rightActivationWithJoinNode/rightActivationWithJoinNode.ts
+++ packages/rete/src/lib/rightActivationWithJoinNode/rightActivationWithJoinNode.ts
@@ -26,9 +26,51 @@ export const rightActivationWithJoinNode = <T>(
       )
     }
   } else {
-    /// Fix this, for some reason if we try to do a `for...of` loop it breaks all the things...
-    // the foreach is creating a ton of allocations, we wanna stop that.
-    node.parent.matches.forEach((match) => {
+    /*** START DEBUGGING CODE ***/
+    const matches = node.parent.matches
+    let ms: any[] = []
+    let i = 0
+    matches.forEach((m) => {
+      ms[i] = [JSON.stringify(m)]
+      i++
+    })
+    i = 0
+    for (const match of matches.values()) {
+      ms[i] = [...ms[i], JSON.stringify(match)]
+      i++
+    }
+    for (let i = 0; i < ms.length; i++) {
+      const fe = ms[i][0]
+      const fo = ms[i][1]
+      console.log(fe == fo)
+    }
+    /*** END DEBUGGING CODE ***/
+
+    // This was originally:
+    // parent.node.matches.forEach( (match) => { ...
+    // Which passes all my tests.
+    // However, I've found that `forEach` calls can allocate a bunch of garbage.
+    // Based on the chrome dev tools this is the current culprit for allocations, and
+    // it's the last forEach in my codebase, so I'd like to remove it.
+    //
+    // However... when I switch to `for(const match of matches.values()) {...`
+    // a bunch of tests break, and I have no idea why!!
+    //
+    // The debugging code above is trying to figure out if there is something that doesn't match up.
+    // The best I could find is that if you have an array like `["a",,"c"]`, then `.forEach()` will
+    // actually skip the second element, where `for..of` will not. But if you have `["a",undefined,"c"] then
+    // both behave correctly.
+    //
+    // BUT, I don't expect to have any "holes" (that's what they call having an empty element in an array) in
+    // these arrays! They're procedurally created! So I'm at a loss! I've got my memory usage
+    // down from ~20MB/second for my angriest tests to ~700kb/second, so it would be nice to
+    // trim it down this last little bit but this is blowing my mind!
+    //
+    // To see the problem you can run the "multiple joins" test in `rete.spec.ts`
+    // or just run `npm run test`
+    //
+    // Here's a link tot hat test: https://github.com/trevordilley/edict/blob/6637b9e7b8cb25c5b8ff257450405b13b142011e/packages/rete/src/lib/rete.spec.ts
+    for (const match of matches.values()) {
       // TODO: We need to find call sites where we need to consolidate the bindings into a match
       const idName = node.idName
       if (
@@ -60,6 +102,7 @@ export const rightActivationWithJoinNode = <T>(
           bindings.binding!
         )
       }
-    })
+    }
+    node.parent.matches.forEach((match) => {})
   }
 }
